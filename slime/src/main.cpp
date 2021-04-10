256ba8d700d2760ee082d62f8dda5b85d60ab586
Hussein Taher <6496177+Husenap@users.noreply.github.com>
2021-04-10 02:34:58 +0200

    use dubu-opengl-app

--- slime/src/main.cpp
+++ slime/src/main.cpp
@@ -1,131 +1,117 @@
+#include <dubu_opengl_app/dubu_opengl_app.hpp>
 #include <dubu_pack/dubu_pack.h>
-#include <dubu_window/dubu_window.h>
+#include <imgui/imgui.h>
 
 #include "slime/Shader.h"
 #include "slime/ShaderProgram.h"
 
-float vertices[6] = {
-    -1.0f,
-    3.0f,
-    3.0f,
-    -1.0f,
-    -1.0f,
-    -1.0f,
-};
+constexpr int WIDTH  = 1920;
+constexpr int HEIGHT = 1080;
 
-void CheckErrors(std::optional<std::string> err) {
-	if (err) {
-		std::cerr << *err << std::endl;
+class SlimeApp : public dubu::opengl_app::AppBase {
+public:
+	SlimeApp()
+	    : dubu::opengl_app::AppBase({.appName = "Slime Simulation"}) {}
+	virtual ~SlimeApp() {}
+
+protected:
+	virtual void Init() override {
+		dubu::pack::Package assetsPackage("assets");
+
+		ComputeShader computeShader(
+		    *assetsPackage.GetFileLocator()->ReadFile("shaders/slime.comp"));
+		CheckErrors(computeShader.GetError());
+
+		mSlimeProgram = std::make_unique<ShaderProgram>(computeShader);
+		CheckErrors(mSlimeProgram->GetError());
+
+		glGenTextures(1, &mImage);
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, mImage);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexImage2D(GL_TEXTURE_2D,
+		             0,
+		             GL_RGBA32F,
+		             WIDTH,
+		             HEIGHT,
+		             0,
+		             GL_RGBA,
+		             GL_FLOAT,
+		             NULL);
+		glBindImageTexture(
+		    0, mImage, 0, GL_FALSE, 0, GL_READ_WRITE, GL_RGBA32F);
 	}
-}
 
-int main() {
-	auto window = std::make_unique<dubu::window::GLFWWindow>(
-	    dubu::window::GLFWWindow::CreateInfo{
-	        .width  = 512,
-	        .height = 512,
-	        .title  = "Slime",
-	        .api    = dubu::window::GLFWWindow::Api::OpenGL,
-	    });
-
-	glfwMakeContextCurrent(window->GetGLFWHandle());
-	if (!gladLoadGL((GLADloadfunc)glfwGetProcAddress)) {
-		std::cerr << "Failed to init glad" << std::endl;
-		return 1;
-	}
-
-	auto resizeToken = window->RegisterListener<dubu::window::EventResize>(
-	    [](const auto& e) { glViewport(0, 0, e.width, e.height); });
-	auto keyPressToken = window->RegisterListener<dubu::window::EventKeyPress>(
-	    [&](const auto& e) {
-		    if (e.key == GLFW_KEY_ESCAPE) {
-			    glfwSetWindowShouldClose(window->GetGLFWHandle(), GLFW_TRUE);
-		    }
-	    });
-
-	glViewport(0, 0, 512, 512);
-	glfwSwapInterval(0);
-
-	GLuint vertexArray;
-	glGenBuffers(1, &vertexArray);
-	glBindBuffer(GL_ARRAY_BUFFER, vertexArray);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-	glGenVertexArrays(1, &vertexArray);
-
-	int    texWidth  = 512;
-	int    texHeight = 512;
-	GLuint texOutput;
-	glGenTextures(1, &texOutput);
-	glActiveTexture(GL_TEXTURE0);
-	glBindTexture(GL_TEXTURE_2D, texOutput);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexImage2D(GL_TEXTURE_2D,
-	             0,
-	             GL_RGBA32F,
-	             texWidth,
-	             texHeight,
-	             0,
-	             GL_RGBA,
-	             GL_FLOAT,
-	             NULL);
-	glBindImageTexture(0, texOutput, 0, GL_FALSE, 0, GL_READ_WRITE, GL_RGBA32F);
-
-	dubu::pack::Package assetsPackage("assets");
-
-	VertexShader copyVertexShader(
-	    *assetsPackage.GetFileLocator()->ReadFile("shaders/quad.vert"));
-	CheckErrors(copyVertexShader.GetError());
-
-	FragmentShader copyFragmentShader(
-	    *assetsPackage.GetFileLocator()->ReadFile("shaders/quad.frag"));
-	CheckErrors(copyFragmentShader.GetError());
-
-	ShaderProgram copyProgram(copyVertexShader, copyFragmentShader);
-	CheckErrors(copyProgram.GetError());
-
-	ComputeShader computeShader(
-	    *assetsPackage.GetFileLocator()->ReadFile("shaders/slime.comp"));
-	CheckErrors(computeShader.GetError());
-
-	ShaderProgram computeProgram(computeShader);
-	CheckErrors(computeProgram.GetError());
-
-	GLint vPosLoc = copyProgram.GetAttributeLocation("vPos");
-
-
-	while (!window->ShouldClose()) {
-		window->PollEvents();
-
-		glBindFramebuffer(GL_FRAMEBUFFER, 0);
-
+	virtual void Update() override {
 		{
-			glUseProgram(computeProgram.mProgram);
-			glDispatchCompute(800, 600, 1);
+			glUseProgram(mSlimeProgram->mProgram);
+			glUniform1f(0, mOffset);
+			glUniform2f(1, WIDTH, HEIGHT);
+			glUniform1f(2, mFadeSpeed);
+			glDispatchCompute(WIDTH, HEIGHT, 1);
 		}
+
+		if (ImGui::Begin("Viewport")) {
+			ImVec2 regionMin = ImGui::GetWindowContentRegionMin();
+			ImVec2 regionMax = ImGui::GetWindowContentRegionMax();
+			ImVec2 offset    = regionMin;
+			ImVec2 regionSize =
+			    ImVec2(regionMax.x - regionMin.x, regionMax.y - regionMin.y);
+			ImVec2 imageSize = {WIDTH, HEIGHT};
+
+			float regionRatio = regionSize.x / regionSize.y;
+			float imageRatio =
+			    static_cast<float>(WIDTH) / static_cast<float>(HEIGHT);
+
+			if (regionRatio > imageRatio) {
+				imageSize.x *= regionSize.y / imageSize.y;
+				imageSize.y = regionSize.y;
+			} else {
+				imageSize.y *= regionSize.x / imageSize.x;
+				imageSize.x = regionSize.x;
+			}
+
+			ImGui::SetCursorPosX((regionSize.x - imageSize.x) * 0.5f +
+			                     offset.x);
+			ImGui::SetCursorPosY((regionSize.y - imageSize.y) * 0.5f +
+			                     offset.y);
+
+			ImGui::Image(reinterpret_cast<void*>(static_cast<intptr_t>(mImage)),
+			             imageSize,
+			             {0, 1},
+			             {1, 0});
+		}
+		ImGui::End();
+
+		if (ImGui::Begin("Parameters")) {
+			ImGui::DragFloat("Offset", &mOffset, 0.01f);
+			ImGui::SliderFloat("FadeSpeed", &mFadeSpeed, 0.0001f, 1.0f);
+		}
+		ImGui::End();
 
 		glMemoryBarrier(GL_SHADER_IMAGE_ACCESS_BARRIER_BIT);
-
-		{
-			glClear(GL_COLOR_BUFFER_BIT);
-			glUseProgram(copyProgram.mProgram);
-			glBindVertexArray(vertexArray);
-			glVertexAttribPointer(vPosLoc,
-			                      2,
-			                      GL_FLOAT,
-			                      GL_FALSE,
-			                      (GLsizei)sizeof(vertices[0]) * 2,
-			                      nullptr);
-			glEnableVertexAttribArray(vPosLoc);
-			glActiveTexture(GL_TEXTURE0);
-			glBindTexture(GL_TEXTURE_2D, texOutput);
-			glDrawArrays(GL_TRIANGLES, 0, 3);
-		}
-
-		window->SwapBuffers();
 	}
+
+private:
+	void CheckErrors(std::optional<std::string> err) {
+		if (err) {
+			std::cerr << *err << std::endl;
+		}
+	}
+
+	std::unique_ptr<ShaderProgram> mSlimeProgram;
+	GLuint                         mImage;
+	float                          mOffset = 0.f;
+	float                          mFadeSpeed = 0.01f;
+};
+
+int main() {
+	SlimeApp app;
+
+	app.Run();
 
 	return 0;
 }
